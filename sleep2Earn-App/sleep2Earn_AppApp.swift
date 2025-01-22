eed20326d6c49e563ec5af108ff4e7e567315d77
maziazi <mazis9651@gmail.com>
2025-01-22 23:20:32 +0700

    Initial Commit

---
+++ sleep2Earn-App/sleep2Earn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleep2Earn_AppApp.swift
+//  sleep2Earn-App
+//
+//  Created by Muhamad Azis on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct sleep2Earn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
