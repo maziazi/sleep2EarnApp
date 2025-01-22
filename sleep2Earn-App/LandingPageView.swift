f7439226b60e9cff65e57c71ecbe46673483a8fb
maziazi <mazis9651@gmail.com>
2025-01-23 02:45:22 +0700

    feat: landing page

--- sleep2Earn-App/LandingPageView.swift
+++ sleep2Earn-App/LandingPageView.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct LandingPageView: View {
+    @State private var showScondImage=false
+    @State private var navigateToHome = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack{
+                Color(hex:"131338")
+                    .ignoresSafeArea()
+                VStack {
+                    if !showScondImage{
+                        Image("logo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                            .padding(.bottom, 20)
+                            .transition(.opacity)
+                    } else {
+                        Image("logoText")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:240, height: 240)
+                            .padding(.bottom, 20)
+                            .transition(.opacity)
+                    }
+                }
+                .padding()
+                .onAppear(){
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                        withAnimation{
+                            showScondImage.toggle()
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 4){
+                        navigateToHome=true
+                    }
+                }
+                .navigationDestination(isPresented: $navigateToHome){
+                    HomeView()
+                }
+            }
+        }
+    }
+}
+
+extension Color{
+    init(hex:String){
+        let scanner = Scanner(string: hex.trimmingCharacters(in: .whitespacesAndNewlines))
+        scanner.currentIndex=hex.hasPrefix("#") ? hex.index(after: hex.startIndex) : hex.startIndex
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        let red = Double((rgbValue>>16)&0xFF)/255.0
+        let green = Double((rgbValue>>8)&0xFF)/255.0
+        let blue = Double((rgbValue&0xFF))/255.0
+        self.init(red: red, green: green, blue: blue)
     }
 }
 
