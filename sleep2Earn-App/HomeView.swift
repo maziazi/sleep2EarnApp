21b683126f0003e2d2fe392da2d94d3acc5e5ef3
maziazi <mazis9651@gmail.com>
2025-01-23 02:43:46 +0700

    landing page

---
+++ sleep2Earn-App/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  sleep2Earn-App
+//
+//  Created by Muhamad Azis on 23/01/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ sleep2Earn-App/LandingPageView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPageView.swift
+//  sleep2Earn-App
+//
+//  Created by Muhamad Azis on 23/01/25.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingPageView()
+}
